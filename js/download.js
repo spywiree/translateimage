8b10dafef5d189bc0e122f74c5fd01854ffbe108
Spywire <79043321+spywiree-priv@users.noreply.github.com>
2024-08-05 16:57:51 +0200

    Lint JS code

--- js/download.js
+++ js/download.js
@@ -2,22 +2,22 @@
  * @param {Blob} blob
  * @returns {Promise<string>}
  */
-async function blobToB64(blob) {
-    return new Promise((resolve) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result);
-        reader.readAsDataURL(blob);
-    });
+async function blobToBase64(blob) {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.readAsDataURL(blob);
+  });
 }
 
 /**
  * @param {string} url
  */
 async function download(url) {
-    var blob = await fetch(url).then(r => r.blob());
-    var b64data = await blobToB64(blob);
-    return {
-        "contentType": blob.type,
-        "b64data": b64data.split(",")[1],
-    };
+  const blob = await fetch(url).then((r) => r.blob());
+  const b64data = await blobToBase64(blob);
+  return {
+    contentType: blob.type,
+    b64data: b64data.split(",")[1],
+  };
 }
